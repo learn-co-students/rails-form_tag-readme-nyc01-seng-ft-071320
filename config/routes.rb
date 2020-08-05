f99d1a03ee32dcff5b292b546cd8217b4c52877b
jztimm <jztimm@gmail.com>
2020-08-05 15:24:41 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,4 @@ Rails.application.routes.draw do
   resources:posts, only: [:new, :create]
 
   get '/posts/:id', to: 'posts#show', as: 'post'
-
-  # get '/posts/new', to: 'posts#new', as: 'new_post'
 end
