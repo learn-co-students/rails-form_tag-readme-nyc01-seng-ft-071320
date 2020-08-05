0372798aa3730038bd6dbc1a8cd7959af115f9d8
github username <joshmrallen@gmail.com>
2020-08-05 17:32:34 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,23 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+  end
+
+  def create
+    @post = Post.create(title: params[:post][:title], description: params[:post][:description])
+    params.require(:post).permit(:title, :description)
+    redirect_to posts_path
+    
+    # @post = Post.new(post_params(:title, :description))
+    # @post.save
+    # redirect_to posts_path(@posts)
+  end
+
+  # private
+  # def post_params(*args)
+  #   params.require(:post).permit(*args)
+  # end
   
 end
